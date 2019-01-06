ec8326607ac0c25d184200c551f50da7fb7c4632
minza.bhat <minza.bhat@kashmirbox.com>
2019-01-06 16:29:33 +0530

    brand page added

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -40,3 +40,16 @@ $(document).ready(function(){
     }
   });
 });
+//--script for sticky menu
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("myHeader");
+var sticky = navbar.offsetTop;
+
+function myFunction() { 
+
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
